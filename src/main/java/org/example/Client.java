ca463a5f3c8fec71a1d3d5ced597272e962c28aa
Artemis Roberts <onevoxiegirl@gmail.com>
2024-02-07 09:51:37 +0000

    retrieve server messages implemented without encryption

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -2,9 +2,20 @@ package org.example;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 public class Client {
+
+
+    private static PrivateKey prvClientKey;
+    private static PublicKey pubClientKey;
+    private static PublicKey pubServerKey;
     public static void main(String[] args) {
         String host = args[0]; // hostname of server
         int port = 0;           // port of server
@@ -18,14 +29,104 @@ public class Client {
         }
 
 
+        try{
+            //reading client private key
+            String path = args[2] + ".prv";
+            File f = new File(path);
+            byte[] keyBytes = Files.readAllBytes(f.toPath());
+            PKCS8EncodedKeySpec prvSpec = new PKCS8EncodedKeySpec(keyBytes);
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            prvClientKey = kf.generatePrivate(prvSpec);
+
+            //reading client public key
+            path = args[2] + ".pub";
+            f = new File(path);
+            keyBytes = Files.readAllBytes(f.toPath());
+            X509EncodedKeySpec pubSpec = new X509EncodedKeySpec(keyBytes);
+            kf = KeyFactory.getInstance("RSA");
+            pubClientKey = kf.generatePublic(pubSpec);
+
+            //reading server public key
+            f = new File("server.pub");
+            keyBytes = Files.readAllBytes(f.toPath());
+            pubSpec = new X509EncodedKeySpec(keyBytes);
+            kf = KeyFactory.getInstance("RSA");
+            pubServerKey = kf.generatePublic(pubSpec);
+
+
+            //print for debugging remove later
+            System.out.println(pubClientKey);
+            System.out.println(prvClientKey);
+            System.out.println(pubServerKey);
+
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e){
+            System.err.println("Cannot read Client keys");
+        }
 
         //attempt to establish connection to server with provided details
         try {
+            //creating socket
             Socket s = new Socket(host, port);
 
+            //hashing uid to hex
+            String secretstring = "gfhk2024:"+args[2]; // secret string
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(secretstring.getBytes());
+            byte[] digest = md.digest();
+            String uidHex = bytesToHexString(digest);
 
+
+            //sending uid to server
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            dos.writeUTF(uidHex);
+
+            //receiving reply from server
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            String reply = dis.readUTF();
+            System.out.println(reply);
+            for (int i = 0; i < (Integer.parseInt(reply)); i++) {
+                //receiving encrypted message from server
+                String ts = dis.readUTF();
+                String encMessage = dis.readUTF();
+                String signatureString = dis.readUTF();
+
+                //print for debugging remove later
+                System.out.println("Timestamp: " + ts);
+                System.out.println("Encrypted Message: " + encMessage);
+                System.out.println("Signature: " + signatureString);
+
+                //verify signature
+                byte[] signatureBytes = hexStringToByteArray(signatureString);
+                Signature sig = Signature.getInstance("SHA256withRSA");
+                sig.initVerify(pubServerKey);
+                sig.update((ts+encMessage).getBytes());
+                if (sig.verify(signatureBytes)) {
+                    System.out.println("Signature verified");
+                } else {
+                    System.out.println("Signature verification failed");
+                    break;
+                }
+            }
         } catch (Exception e) {
             System.err.println("Cannot connect to server.");
         }
+    }
+
+    public static String bytesToHexString(byte[] b) {
+        StringBuilder sb = new StringBuilder();
+        for (byte x : b) {
+            sb.append(String.format("%02X", x));
+        }
+        return sb.toString();
+    }
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] b = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            b[i / 2] = (byte)((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i+1), 16));
+        }
+        return b;
     }
 }
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -2,13 +2,28 @@ package org.example;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
+import java.util.Date;
 
 public class Server {
+
+    private static PrivateKey prvServerKey;
+    private static PublicKey pubServerKey;
+    private static String[][] messages;
+
     public static void main(String[] args) throws IOException {
-        System.out.println("hello world");
+
+
 
 
         //fetching port from command line arguments
@@ -19,18 +34,127 @@ public class Server {
             System.err.println("Port must be an integer.");
         }
 
-        //starting server
         try{
-            ServerSocket ss = new ServerSocket(port);
+            //reading server private key
+            File f = new File("server.prv");
+            byte[] keyBytes = Files.readAllBytes(f.toPath());
+            PKCS8EncodedKeySpec prvSpec = new PKCS8EncodedKeySpec(keyBytes);
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            prvServerKey = kf.generatePrivate(prvSpec);
+
+            //reading server public key
+            f = new File("server.pub");
+            keyBytes = Files.readAllBytes(f.toPath());
+            X509EncodedKeySpec pubSpec = new X509EncodedKeySpec(keyBytes);
+            kf = KeyFactory.getInstance("RSA");
+            pubServerKey = kf.generatePublic(pubSpec);
+
+
+            //print for debugging remove later
+            System.out.println(pubServerKey);
+            System.out.println(prvServerKey);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e){
+            System.err.println("Cannot read server keys");
+        }
+        //starting server
+
+        try{
+            ServerSocket serverSocket = new ServerSocket(port);
             System.out.println("Waiting incoming connection...");
-            Socket s = ss.accept();
-
-
+            while (true) {
+                new ClientHandler(serverSocket.accept()).start();
+            }
         } catch (Exception e) {
             System.err.println("Cannot start server");
+            System.err.println(e.getMessage());
+
         }
 
+
+
+    }
+
+    private static class ClientHandler extends Thread {
+        private Socket clientSocket;
+
+        private String uid;
+
+
+        public ClientHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        public void run() {
+            try {
+                DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+                DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
+                String readData = null;
+                readData = dis.readUTF();
+                String uid = readData;
+                System.out.println(readData);
+                messages = new String[1][3];
+                messages[0] = new String[]{uid, "Hello", "2021-10-10 10:10:10"};
+                try {
+                    int messageCount = 0;
+                    for (int i = 0; i < messages.length; i++) {
+                        if (messages[i] != null && messages[i][0].equals(uid)) {
+                            messageCount++;
+                        }
+                    }
+                    dos.writeUTF(Integer.toString(messageCount));
+
+                    //search for messages
+                    for (int i = 0; i < messages.length; i++) {
+                        if (messages[i] != null && messages[i][0].equals(uid)) {
+                            //send message and timestamp
+                            dos.writeUTF(messages[i][2].toString());
+                            dos.writeUTF(messages[i][1].toString());
+                            //sign message
+                            String signatureString  =  messages[i][2]+ messages[i][1];
+                            byte[] signature = signatureString.getBytes();
+                            Signature sign = Signature.getInstance("SHA256withRSA");
+                            sign.initSign(prvServerKey);
+                            sign.update(signature);
+                            byte[] signatureBytes = sign.sign();
+                            //send signature
+                            dos.writeUTF(bytesToHexString(signatureBytes));
+                            //remove message from memory
+                            messages[i] = null;
+                        }
+                    }
+                } catch (NullPointerException e) {
+                    dos.writeUTF("0");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+
+
+
+    public static String bytesToHexString(byte[] b) {
+        StringBuilder sb = new StringBuilder();
+        for (byte x : b) {
+            sb.append(String.format("%02X", x));
+        }
+        return sb.toString();
+    }
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] b = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            b[i / 2] = (byte)((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i+1), 16));
+        }
+        return b;
     }
 
 
 }
+
